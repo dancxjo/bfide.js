f5045bf3b0a287dcd2e89ddec517c0e2c0b178c9
dancxjo <tdreed@gmail.com>
2014-08-21 10:36:49 -0700

    Create bf.php

---
+++ bf.php
@@ -0,0 +1,113 @@
+<?php
+
+class Script {
+	protected $source = "";
+	protected $sp = 0;
+	protected $input;
+	protected $ip = 0;
+	protected $output;
+	protected $data;
+	protected $dp = 0;
+	protected $callstack;	
+	
+	function __construct($source, $input=null) {
+		$this->source = $source;
+		$this->input = $input === null ? array() : $input;
+		$this->output = array();
+		$this->data = array();
+		$this->callstack = array();
+	}
+	
+	function execute() {
+		$n = 0;
+		while ($this->sp < strlen($this->source)) {
+			$this->step();
+			$n++;
+		}
+		return $n;
+	}
+	
+	function step() {
+		if ($this->sp < strlen($this->source)) {
+			$this->opExec($this->getCurOp());
+			$this->sp++;
+		}
+	}
+	
+	function getCurOp() {
+		return $this->source[$this->sp];
+	}
+	
+	function skipForward() {
+		$depth = 0;
+		$i = $this->sp;
+		do {
+			if ($i >= strlen($this->source)) {
+				throw new Exeception("Unmatched [ at " . $this->sp);
+			}
+			$c = $this->source[$i++];
+			if ($c == '[') $depth++; else if ($c == ']') $depth--;			
+		} while ($depth > 0);
+		
+		$this->sp = $i;
+	}
+	
+	function opExec($op) {		
+		switch ($op) {
+			case ">":
+			$this->dp++;
+			break;
+			
+			case "<":
+			$this->dp--;
+			break;
+			
+			case "+":
+			$this->data[$this->dp]++;
+			break;
+			
+			case "-":
+			$this->data[$this->dp]--;
+			break;
+			
+			case ".":
+			$this->output[] = $this->data[$this->dp];
+			break;
+			
+			case ",":
+			$this->data[$this->dp] = $this->input[$this->ip++];
+			break;
+			
+			case "[":
+			if (!$this->data[$this->dp]) {
+				$this->skipForward();
+			} else {
+				array_push($this->callstack, $this->sp);
+			}
+			break;
+			
+			case "]":
+			if (!$this->data[$this->dp]) {
+				array_pop($this->callstack);
+			} else {
+				$this->sp = $this->callstack[count($this->callstack) - 1];
+			}
+			break;
+		}		
+	}
+	
+	function getOutput() {
+		return $this->output;
+	}
+}
+
+$start = microtime();
+$script = new Script(",>,[-<+>]<.", array(2, 2));
+$steps = $script->execute();
+echo json_encode(array(
+	'duration'	=>	microtime()-$start, 
+	'result'	=>	$script->getOutput(), 
+	'steps'		=>  $steps
+));
+
+?>
