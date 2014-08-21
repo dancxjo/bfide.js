dc381ba8b382b2398787362589b598491b944f39
dancxjo <tdreed@gmail.com>
2014-08-21 10:59:36 -0700

    Update flower.js

--- flower.js
+++ flower.js
@@ -1,9 +1,23 @@
-function Flower(genFruit) {
+function Fruit(input, output) {
+  this.input = input;
+  this.output = output;
+}
+
+function Flower(genFruit, fitness) {
   this.genFruit = genFruit;
+  this.fitness = fitness;
+}
+
+Flower.prototype.test = function (script) {
+  var fruit = this.genFruit();
+  var results = script.execute(fruit.input);
+  return this.fitness(results, fruit.output);
 }
 
 var adder = new Flower(function () {
   var a = Math.floor(Math.random() * 255);
   var b = Math.floor(Math.random() * 255);
-  return [[a, b], [a+b]];
+  return new Fruit([a, b], [a+b]);
+}, function (results, output) {
+  return output[0] - results.output[0] / results.time;
 });
